065de17b3c867a71c236ad0fdd5e6a87bb02ba9c
cfarot <charlyfarot@gmail.com>
2014-05-29 16:40:21 +0200

    init : specs + fonctions tests Todo

---
+++ Projects/nurl/NURL/NURL/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Charly-PC
+ * Date: 29/05/2014
+ * Time: 16:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NURL
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Projects/nurl/NURL/NURL/TestNURL.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Charly-PC
+ * Date: 29/05/2014
+ * Time: 16:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace NURL
+{
+	[TestFixture]
+	public class TestNURL
+	{
+		[Test]
+		public void Should_return_false_with_a_fake_URL()
+		{
+			// TODO: Add your test.
+		}
+		
+		[Test]
+		public void Should_return_true_with_a_true_URL()
+		{
+			// Todo
+		}
+		
+		[Test] 
+		public void The_result_should_be_a_string()
+		{
+			// Todo
+		}
+		
+		[Test]
+		public void Content_fake_url()
+		{
+			// Todo
+		}
+		
+		[Test]
+		public void Content_like_browser()
+		{
+			// Todo
+		}
+	}
+}
